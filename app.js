c48964377d10281a8d3784c6f00845955abb9adf
KrisO95 <kristianobrien95@gmail.com>
2024-12-20 17:01:03 -0500

    updated html and css, created some visual changes havent done any javascript yet

---
+++ app.js
@@ -0,0 +1,17 @@
+/*-------------- Constants -------------*/
+
+const card = document.querySelectorAll('.card')
+console.log(card)
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+
+
