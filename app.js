ae34b980a3be32bb6fd84d52b1f1af0c843eb07e
KrisO95 <kristianobrien95@gmail.com>
2025-01-09 15:56:22 -0500

    added logic for showing a brief preview of the cards when game starts to make it easier for the user cleaned up uncommented code

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const card = document.querySelectorAll('.card');
 const playButton = document.getElementById('play-again-btn')
 const totalPairs = 6;
 
-// const gameBoard = document.getElementById('gameBoard')
-
 /*---------- Variables (state) ---------*/
 
 let cards = Array.from(document.querySelectorAll('.card'));
@@ -22,21 +20,22 @@ function shuffleCards() {
   cards.sort(() => Math.random() - 0.5);
   
   const gameBoard = document.querySelector('.game-board');
-  cards.forEach(card => gameBoard.appendChild(card)); // Reorder the cards
+  cards.forEach(card => gameBoard.appendChild(card));
 }
 
 shuffleCards()
 
 function resetGame() {
-  flippedCards = [];//Reset the flipped card Array
+    flippedCards = [];
   cards.forEach(card => {
-    card.querySelector('.card-inner').classList.remove('flipped'); //The cards that are already flipped gets reset and flipped
-    card.classList.remove('hidden'); //Make sure the cards are hidden.
-    matchedPairs = 0; //Added the matched pairs to equal 0 so when the play again button is hidden the win message gets removed.
+    card.querySelector('.card-inner').classList.remove('flipped');
+    card.classList.remove('hidden');
+    matchedPairs = 0;
     document.getElementById('win-message').classList.add('hidden');
   });
 
   shuffleCards();
+  showbrief();
 }
 
 playButton.addEventListener('click', resetGame);
@@ -47,32 +46,35 @@ function checkWinCondition() {
   }
 }
 
-/*----------- Event Listeners ----------*/
+function showbrief() {
+  card.forEach(card => {
+    card.querySelector('.card-inner').classList.add('flipped');
+  })
+  setTimeout(() => {
+    card.forEach(card => {
+      card.querySelector('.card-inner').classList.remove('flipped');
+    })
+  },2000);
+}
 
-playButton.addEventListener('click', (resetGame));
+
+/*----------- Event Listeners ----------*/
 
 cards.forEach(card => {
   card.addEventListener('click', () => {
     if (flippedCards.length < 2 && !card.querySelector('.card-inner').classList.contains('flipped')) {
       flippedCards.push(card);
       card.querySelector('.card-inner').classList.add('flipped');
-      
-      // console.log(`Flipped Cards: ${flippedCards.map(card => card.dataset.id).join(', ')}`);
-
-      if (flippedCards.length === 2) {
+        if (flippedCards.length === 2) {
         const [firstCard, secondCard] = flippedCards;
-        
-        // console.log(`Comparing cards with IDs: ${firstCard.dataset.id} and ${secondCard.dataset.id}`);
-
         if (firstCard.dataset.id === secondCard.dataset.id) {
-          // console.log('Match found!');
-          matchedPairs++;//Added the macthedPair here so it can check to see if all cards are matched and log winner!
+          matchedPairs++;
           checkWinCondition();
         } else {
           setTimeout(() => {
             firstCard.querySelector('.card-inner').classList.remove('flipped');
             secondCard.querySelector('.card-inner').classList.remove('flipped');
-          }, 1000); // Wait 1 second before flipping back
+          }, 1000);
         }
 
         flippedCards = [];
